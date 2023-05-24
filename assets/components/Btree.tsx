d8ace9695baaed4f2d17bf2078446f0929cee92a
AlexRussDev <russakov.aleksandr@gmail.com>
2023-05-24 16:04:41 +0300

    Refactoring

--- assets/components/Btree.tsx
+++ assets/components/Btree.tsx
@@ -7,7 +7,9 @@ import React from 'react';
 
 const API_URL = 'http://127.0.0.1:8000/api/';
 
+
 export const Btree = (props: any) => {
+  
   const [loaded, setLoaded] = useState<boolean>(false);
   const [btreeData, setBtreeData] = useState<any>([])
 
@@ -18,6 +20,7 @@ export const Btree = (props: any) => {
       }).catch((err) => console.log(err))
   }, [])
 
+
   const fetchBtreeData = () => {
     return axios.get(API_URL + 'btree')
   }
@@ -25,28 +28,25 @@ export const Btree = (props: any) => {
   if (!loaded) return null;
  
 
-
- const RecursiveComponent = ({ data, parentGuid }: any) => {
-  
-  const activeNodeData = data.filter((element: any) => element.parentguid == parentGuid)
-  return (
-    <React.Fragment key={nanoid(11)}>
-      {Object.entries(activeNodeData).map(([_, item]: any) => {
-      
-         return (
-          <React.Fragment key={nanoid(11)}>
-            {typeof(activeNodeData) !== 'undefined' && item.hasChildren > 0 ?
-            (
-            <StyledTreeItem key={nanoid(11)} nodeId={item.guid} label={item.name} >
-              { <RecursiveComponent key={nanoid(11)} data={ data} parentGuid={item.guid}/>}
-              </StyledTreeItem>
-              ) : (<StyledTreeItem key={nanoid(11)} nodeId={item.guid + '_' + nanoid(3)} label={item.name} />)
-            }
-          </React.Fragment>
-        );
-      })}
-      </React.Fragment>
-  );
+  const RecursiveComponent = ({ data, parentGuid }: any) => {
+  const activeNodeData = data.filter((element: any) => element.parentguid == parentGuid);
+    return (
+      <React.Fragment key={nanoid(11)}>
+        {Object.entries(activeNodeData).map(([_, item]: any) => {
+          return (
+            <React.Fragment key={nanoid(11)}>
+              {typeof(activeNodeData) !== 'undefined' && item.hasChildren > 0 ?
+              (
+              <StyledTreeItem key={nanoid(11)} nodeId={item.guid} label={item.name} >
+                { <RecursiveComponent key={nanoid(11)} data={ data} parentGuid={item.guid}/>}
+                </StyledTreeItem>
+                ) : (<StyledTreeItem key={nanoid(11)} nodeId={item.guid + '_' + nanoid(3)} label={item.name} />)
+              }
+            </React.Fragment>
+          );
+        })}
+        </React.Fragment>
+    );
 };
 
   return (<div style={props.style}><CustomizedTreeView key={nanoid(11)} children={RecursiveComponent({data: btreeData, parentGuid: null})}></CustomizedTreeView></div>);
